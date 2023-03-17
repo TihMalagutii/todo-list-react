337e5099f30460514671de98a9316e3bb2e238fe
TihMalagutii <euphoriabeats7146090532@gmail.com>
2023-03-17 02:33:02 -0300

    docs: :memo: update docs names

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import styles from './home.module.css'
+import styles from './login.module.css'
 
 export default function Home() {
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom"
 
-import Home from "../pages/Home"
+import Login from "../pages/Login"
 import Register from "../pages/Register"
 
 function RoutesApp(){
     return(
         <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Login />} />
             <Route path="/register" element={<Register />} />
         </Routes>
     )
