07fd8fc3f1df3380fa3ca8b2aeab0bfca3e631f4
TihMalagutii <euphoriabeats7146090532@gmail.com>
2023-03-17 02:27:52 -0300

    feat: :white_check_mark: login page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,50 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import styles from './home.module.css'
 
 export default function Home() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const login = e => {
+
+    e.preventDefault()
+
+    if(email !== '' && password !== '') {
+      alert('test')
+    } else {
+      alert('error login')
+    }
+
+  }
+
   return (
-    <div>Home</div>
+    <div className={styles.container}>
+      <h1>To Do List</h1>
+      <span>Manage your tasks!</span>
+
+      <form className={styles.formLogin} onSubmit={login}>
+
+        <input 
+          type="text" 
+          placeholder='Email...' 
+          value={email}
+          onChange={e => setEmail(e.target.value)} />
+        <input 
+          type="password" 
+          placeholder='Password...' 
+          value={password}
+          onChange={e => setPassword(e.target.value)} />
+
+          <button type='submit'>Login</button>
+
+      </form>
+
+      <Link className={styles.link} to="/register">
+        Don't have an account? Register!
+      </Link>
+
+    </div>
   )
 }
