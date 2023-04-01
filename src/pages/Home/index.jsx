336edb5eff40a9aadfc452c6208976b7951f294a
TihMalagutii <euphoriabeats7146090532@gmail.com>
2023-04-01 16:12:21 -0300

    feat: :white_check_mark: home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,51 @@
+import { useState } from 'react'
+import { BsFillPencilFill, BsFillCheckCircleFill } from 'react-icons/bs'
+import { FiLogOut } from 'react-icons/fi'
+import { auth } from '../../firebaseConnection'
+import { signOut } from 'firebase/auth'
 import styles from './home.module.css'
 
 export default function Home() {
+
+  const [taskInput, setTaskInput] = useState('')
+
+  const addTask = e => {
+    e.preventDefault()
+    alert('clicou')
+  }
+
+  const logout = () => {
+    signOut(auth)
+  }
+
   return (
-    <div>Home</div>
+    <div className={styles.container}>
+
+      <h1>My tasks</h1>
+
+      <form onSubmit={addTask}>
+        <input
+          placeholder='type a task...'
+          value={taskInput}
+          onChange={e => setTaskInput(e.target.value)}
+        />
+        <button type='submit'>Add task</button>
+      </form>
+
+      <div className={styles.list}>
+
+        <article>
+          <p>task 1</p>
+          <div>
+            <button className={styles.btnEdit}><BsFillPencilFill /></button>
+            <button className={styles.btnConclude}><BsFillCheckCircleFill /></button>
+          </div>
+        </article>
+
+      </div>
+
+      <button className={styles.btnLogout} onClick={logout}><FiLogOut /></button>
+
+    </div>
   )
 }
