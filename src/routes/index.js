a5f67e7597bb9b1dfa9c6d72cb248a4bcd8b9751
TihMalagutii <euphoriabeats7146090532@gmail.com>
2023-03-31 21:22:38 -0300

    feat: :white_check_mark: auth control

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,14 @@ import { Routes, Route } from "react-router-dom"
 import Login from "../pages/Login"
 import Register from "../pages/Register"
 import Home from "../pages/Home"
+import Private from "./Private"
 
 function RoutesApp(){
     return(
         <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/home" element={<Home />} />
+            <Route path="/home" element={<Private> <Home /> </Private>} />
         </Routes>
     )
 }
